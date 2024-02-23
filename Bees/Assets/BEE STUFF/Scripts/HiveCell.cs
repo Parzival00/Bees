3528281c08283ded599e2cfda0944087ad467150
LazyDragonStudios <ainsworth.j.alexis@gmail.com>
2024-02-23 18:04:18 -0500

    Interactions
    My oculus died but im pretty sure the interactons are working now (well with the cells at least) the Testing UI does not have script on it. but you can grab and insert items into the cells to change their state.

--- Bees/Assets/BEE STUFF/Scripts/HiveCell.cs
+++ Bees/Assets/BEE STUFF/Scripts/HiveCell.cs
@@ -1,7 +1,9 @@
+using Oculus.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 
 public enum CellState { Empty, HoneyInside, larveInside, Capped}
@@ -12,26 +14,57 @@ public class HiveCell : MonoBehaviour
     public GameObject HoneyInside;
     public GameObject larveInside;
 
-    public void ON_Vr_Interact(string playerInput = "Empty")
+    public GameObject HoneyPickUP;
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        BeeGame_Grab InputItem = collision.gameObject.GetComponent<BeeGame_Grab>();
+        if (InputItem != null)
+        {
+            Debug.Log("Collision detected");
+            if(InputItem.being_Held)
+            {
+                ItemEnteredIntoCombCell(InputItem);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        XRDirectInteractor hand = other.gameObject.GetComponent<XRDirectInteractor>();
+        if (hand != null)
+        {
+            ItemEnteredIntoCombCell();
+        }
+    }
+    public void ItemEnteredIntoCombCell(BeeGame_Grab item =null)
     {
         //if player is empty handed, and state is Honey Inside, put honey in player's hand
-        if (currCellState == CellState.HoneyInside && playerInput == "Empty")
+        if (currCellState == CellState.HoneyInside && item == null)
         {
-            // spawn a honey drop above the cell
+            Instantiate(HoneyPickUP, transform.position , Quaternion.identity);
             NewCellState(CellState.Empty);
         }
 
-
-        else if (currCellState == CellState.HoneyInside && playerInput == "Larvae")
+        else if (currCellState == CellState.HoneyInside && item.item_Name == GrabNames.Larvae)
         {
-            //remove the larve from the player's hand
+            Destroy(item.gameObject);
             NewCellState(CellState.larveInside);
         }
+
         //if player input is honey and state is LArve Inside, cap it and remove the script
-        else if (currCellState == CellState.larveInside && playerInput == "Honey")
+        else if (currCellState == CellState.larveInside && item.item_Name == GrabNames.Honey)
         {
             //remove honey from player's hand
+            Destroy(item.gameObject);
             NewCellState(CellState.Capped);
+        }
+
+        else if(currCellState == CellState.Empty && item.item_Name == GrabNames.Nectar)
+        {
+            Destroy(item.gameObject);
+            NewCellState(CellState.HoneyInside);
         }
     }
     public void NewCellState(CellState cellState)
---
+++ Bees/Assets/BeeGame_Grab.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GrabNames {Empty, Nectar, Pollen, Larvae, Honey}
+public class BeeGame_Grab : MonoBehaviour
+{
+    public GrabNames item_Name;
+    public MiniGameScriptable[] miniGamesUsed;
+    public bool being_Held;
+
+    public void ON_SelectEntered()
+    {
+        being_Held = true;
+    }
+    public void On_SelectExited()
+    {
+        being_Held = false;
+    }
+}
