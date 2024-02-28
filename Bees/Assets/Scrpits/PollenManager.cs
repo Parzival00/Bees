a0d2df82e02015fab904ec0aec9e9e7e3bf42353
Jesse Israel <jesleads15@gmail.com>
2024-02-27 19:00:26 -0500

    change

--- Bees/Assets/Scrpits/PollenManager.cs
+++ Bees/Assets/Scrpits/PollenManager.cs
@@ -5,22 +5,69 @@ using TMPro;
 
 public class PollenManager : MonoBehaviour
 {
-    //The Player's Pollen
+    public Transform playerCamera;
     public Pollen playerPollen;
+
+    private float detectDistance = 10;
+
+    public GameObject[] Flowers;
+    public GameObject currentFlower;
+    public GameObject PollenMenu = null;
     public TMP_Text pollenCounterText;
 
-    // Start is called before the first frame update
+    public bool nearFlower;
+
     void Start()
     {
-        //player's pollen starts at zero
         playerPollen.count = 0;
         Debug.Log("Current Pollen: " + getPollenCount());
+
+        Flowers = GameObject.FindGameObjectsWithTag("Flower");
+        if (Flowers.Length == 0)
+        {
+            Debug.LogWarning("No flowers found in the scene.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        pollenCounterText.SetText("Pollen: " + getPollenCount());
+        getCurrentFlower();
+
+        if (nearFlower && PollenMenu != null)
+        {
+            PollenMenu.transform.LookAt(playerCamera);
+            pollenCounterText.SetText("Pollen: " + getPollenCount());
+        }
+    }
+
+    public void getCurrentFlower()
+    {
+        foreach (GameObject f in Flowers)
+        {
+            float flowerDistance = Vector3.Distance(transform.position, f.transform.position);
+            if (flowerDistance <= detectDistance)
+            {
+                currentFlower = f;
+                PollenMenu = currentFlower.transform.GetChild(0).gameObject;
+
+                if (PollenMenu != null)
+                {
+                    pollenCounterText = PollenMenu.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
+                    PollenMenu.SetActive(true);
+                    nearFlower = true;
+                }
+                else
+                {
+                    Debug.LogWarning("PollenMenu or its child objects are null.");
+                }
+
+                return;  // Exit the loop once a flower is found
+            }
+        }
+
+        // No flower found, set variables accordingly
+        PollenMenu?.SetActive(false);
+        nearFlower = false;
     }
 
     public void givePollen()
@@ -41,3 +88,4 @@ public class PollenManager : MonoBehaviour
         return playerPollen.count;
     }
 }
+
