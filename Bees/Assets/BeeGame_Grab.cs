f1d7a1283b71aca77fcdcc4a6d5cc6f25e3fff2a
Jesse Israel <jesleads15@gmail.com>
2024-03-02 19:58:26 -0500

    ch

--- Bees/Assets/BeeGame_Grab.cs
+++ Bees/Assets/BeeGame_Grab.cs
@@ -8,10 +8,22 @@ public class BeeGame_Grab : MonoBehaviour
     public GrabNames item_Name;
     public MiniGameScriptable[] miniGamesUsed;
     public bool being_Held;
+    public bool deleteOnGrab;
 
     public void ON_SelectEntered()
     {
         being_Held = true;
+        if (deleteOnGrab)
+        {
+            //add info before destroy (refrence UI to add point for pollen and MnigameManager)
+
+            //MinigameManager info
+            //Total collected
+            //total pollenated
+            //currently carry
+
+            Destroy(gameObject);
+        }
     }
     public void On_SelectExited()
     {
