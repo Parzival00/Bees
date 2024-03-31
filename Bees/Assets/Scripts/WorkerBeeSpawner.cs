72a709d09c2f6875a2376df4ea6e718774f4ff97
LazyDragonStudios <ainsworth.j.alexis@gmail.com>
2024-03-31 18:57:00 -0400

    Fixed Birth Scene to be more engaging
    and less seizure enducing

--- Bees/Assets/Scripts/WorkerBeeSpawner.cs
+++ Bees/Assets/Scripts/WorkerBeeSpawner.cs
@@ -17,7 +17,7 @@ public class WorkerBeeSpawner : MonoBehaviour
             Instantiate(WorkerBeeGO, transform.position,Quaternion.identity);
         }
     }
-    [ContextMenu("Start Mini Game")]
+
     public void SpawnWorkerBee()
     {
         Instantiate(WorkerBeeGO, transform.position, Quaternion.identity);
