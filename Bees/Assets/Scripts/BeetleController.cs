904da59fbe7e1021b316c4b172b2584db76627e8
Ben <benjoehedges@gmail.com>
2024-04-11 12:26:37 -0400

    Pest Control Logic Done (?)
    The logic for corralling the beetles is complete. It just needs to be connected to  the UI and scoring systems

--- Bees/Assets/Scripts/BeetleController.cs
+++ Bees/Assets/Scripts/BeetleController.cs
@@ -6,7 +6,7 @@ using UnityEngine.AI;
 public class BeetleController : MonoBehaviour
 {
     [SerializeField] NavMeshAgent navAgent;
-    [SerializeField] Transform escapeLocation;
+    [SerializeField] Vector3 escapeLocation;
     [SerializeField] float timeToLayEggs;
     [SerializeField] float distanceToKeepFromPlayer;
 
@@ -15,19 +15,41 @@ public class BeetleController : MonoBehaviour
     float timer = 0f;
 
     GameObject player ;
+
+    Vector3 goal;
     
     // Start is called before the first frame update
     void Start()
     {
         //get random cell and make that the target destination for the navmesh
         GetNewDestination();
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player").transform.GetChild(3).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, navAgent.destination) < 1f) //checks if the beetle is within a reasonable radius to the targeted comb
+        //print(Vector3.Distance(transform.position, escapeLocation));
+
+        if (Vector3.Distance(transform.position, player.transform.position) <= distanceToKeepFromPlayer) //if within radius of the player, run away
+        {
+            //print("running away");
+            navAgent.destination = escapeLocation;
+        }
+        else if (Vector3.Distance(transform.position, player.transform.position) >= distanceToKeepFromPlayer * 1.5f)
+        {
+            //print("not running away");
+            navAgent.destination = goal;
+        }
+            
+
+        if (Vector3.Distance(transform.position, escapeLocation) < 5) //destroy if it gets to the escape location
+        {
+            //print("escaping");
+            Destroy(gameObject);
+        }
+
+        if (Vector3.Distance(transform.position, navAgent.destination) < 1f) //checks if the beetle is within a reasonable radius to the targeted comb
         {
             timer += Time.deltaTime;
 
@@ -43,36 +65,24 @@ public class BeetleController : MonoBehaviour
         {
             timer = 0;
         }
-
-        if(Vector3.Distance(transform.position, player.transform.position) < distanceToKeepFromPlayer) //if within radius of the player, run away
-        {
-            navAgent.destination = escapeLocation.position;
-        }
-
-        if(Vector3.Distance(transform.position, escapeLocation.position) < distanceToKeepFromPlayer) //destroy if it gets to the escape location
-        {
-            Destroy(gameObject);
-        }
-
-
     }
 
     /// <summary>
     /// Gets the location of a random open honey comb in the hive
     /// </summary>
-    Vector3 GetNewDestination()
+    void GetNewDestination()
     {
         HiveCell[] cells = FindObjectsByType<HiveCell>(FindObjectsSortMode.None);
         int index = 0;
         Vector3 pos = Vector3.zero;
         //gets the location of the first cell that is uncapped 
-        do
-        {
+        //do
+        //{
             index = Random.Range(0, cells.Length);
             pos = cells[index].gameObject.transform.position;
-        }
-        while (cells[index].currCellState != CellState.Capped);
-
-        return pos;
+        //}
+        //while (cells[index].currCellState != CellState.Capped);
+        goal = pos;
+        navAgent.destination = pos;
     }
 }
